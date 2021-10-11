5f90b2b11d73bddd5e400d27417e7b355f876432
kuoaid <awang8396@gmail.com>
2021-10-11 16:19:59 -0500

    arrow

--- vector.c
+++ vector.c
@@ -6,14 +6,14 @@
 
 //given memory size and vector, make vector.
 void init(Vector *vector, int memorySize) {
-    (*vector).memorySize = memorySize;
-    (*vector).size = 0;
-    (*vector).array = (int*)malloc(memorySize);
+    vector->memorySize = memorySize;
+    vector->size = 0;
+    vector->array = malloc(vector->memorySize*sizeof(int));
 }
 
 void print(Vector *vector){
-    for (int i = 0; i < (*vector).size; i++) {
-        int printed_num = (*vector).array[i];
+    for (int i = 0; i < vector->size; i++) {
+        int printed_num = vector->array[i];
         printf("%i",printed_num);
     }
     printf("\n");
@@ -23,66 +23,63 @@ void print(Vector *vector){
 int insert(Vector *vector, int location, int value) {
 
     //test for invalid location
-    if(location < 0 || location > (*vector).size){
+    if(location < 0 || location > vector->size){
         return 0;
     }
 
-    //determine future size.
-    int future_mem_size = sizeof(value)+sizeof(*vector);
-
     //If future size is bigger
-    if(future_mem_size>(*vector).memorySize){
+    if((sizeof(value) + sizeof(*vector)) > vector->memorySize){
         //update the recorded memory size.
-        (*vector).memorySize = (*vector).memorySize*2;
+        vector->memorySize = vector->memorySize*2;
 
         //allocate a new array in memory with updated memorySize and name the pointer newArray.
-        int* newArray = (int*)malloc((*vector).memorySize);
+        int* newArray = (int*) malloc(vector->memorySize*sizeof(int));
 
         //copy over from old to new.
-        for(int i = 0; i<(*vector).size; i++){
-            newArray[i]=(*vector).array[i];
+        for(int i = 0; i<vector->size; i++){
+            newArray[i] = vector->array[i];
         }
         
         //now that it's copied over, free it.
-        free((*vector).array);
+        free(vector->array);
 
         //update pointer to the new array.
-        (*vector).array = newArray;
+        vector->array = newArray;
     }
 
     //actual insert with correct input AND memory size.
     //from the last one + 1 = size, copy over the previous value.
     //when j = location, stop.
-    for(int j = (*vector).size; j > location; j--){
-        (*vector).array[j]=(*vector).array[j-1];
+    for(int j = vector->size; j > location; j--){
+        vector->array[j]=vector->array[j-1];
     }
     //then, set the location to the value.
-    (*vector).array[location] = value;
+    vector->array[location] = value;
 
     //update size.
-    (*vector).size++;
+    vector->size++;
     return 1;
 }
 
 int get(Vector *vector, int location, int *value){
-    if(location == 0 || location >= (*vector).size){
+    if(location == 0 || location >= vector->size){
         return 0;
     }
-    *value = (*vector).array[location];
+    *value = vector->array[location];
     return 1;
 }
 
 void cleanup(Vector *vector){
-    free((*vector).array);
+    free(vector->array);
 }
 
 int delete(Vector *vector, int location){
-    if(location < 0 || location >= (*vector).size){
+    if(location < 0 || location >= vector->size){
         return 0;
     }
-    for(int i = location; i<(*vector).size-1; i++){
-        (*vector).array[i]=(*vector).array[i+1];
+    for(int i = location; i < vector->size-1; i++){
+        vector->array[i]=vector->array[i+1];
     }
-    (*vector).size--;
+    vector->size--;
     return 1;
-}
+}   
